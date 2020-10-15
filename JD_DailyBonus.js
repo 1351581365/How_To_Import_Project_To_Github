f34406c4b009fb1ed30bc05127b9397138ee488a
lxk0301 <778732665@qq.com>
2020-10-15 10:12:55 +0800

    Update JD_DailyBonus.js

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -1941,9 +1941,12 @@ function JDTakeaLook(s) {
           console.log(`野比tm:::${tm}`)
           console.log(`京东服务器返回的时间戳${cc.data.discTasks[0].signDetail.discTaskItemInfos[3].handleTime}`)
           const tm1 = zone == -480 ? new Date().setHours(0, 0, 0, 0) : new Date(Date.now() - 28800000).setHours(0, 0, 0, 0);
+          const tm2 = zone == -480 ? new Date().setHours(0, 0, 0, 0) : new Date(Date.now()).setHours(0, 0, 0, 0);
           console.log(`tm1(减去8小时):::${tm1}`)
+          console.log(`tm2(不加不减):::${tm2}`)
           console.log(`野比data.match(tm):${data.match(tm)}`)
           console.log(`data.match(tm1):${data.match(tm1)}`)
+          console.log(`data.match(tm2):${data.match(tm2)}`)
           if (zone !== -480 && zone !== 0) throw new Error('非UTC+8时区, 签到结果未知.');
           if (data.match(/签到成功/) && !data.match(tm)) {
             console.log(`\n京东发现-看看签到成功 ${Details}`)
